1d60d43f161f3a526d693a1dd18b824d9eca9c8d
romain <romain@LAPTOP-CHT7AOJR>
2019-09-05 11:54:43 +0200

    utiliser les condition if, if/else, if/else if/else

---
+++ formation/src/main/java/fr/formation/o7/IfElse.java
@@ -0,0 +1,40 @@
+package fr.formation.o7;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// condition: Des conditions doivent être vérifiées.
+		if (true)  {
+		    // Faites quelque chose ici si la condition est vraie.
+		}
+		
+        // Déclarez une variable représentant votre âge
+        int age = 30;
+ 
+        System.out.println("Your age: " + age);
+ 
+        // La condition (condition) à vérifier est 'age==>== 20'
+        if (age > 20) {
+ 
+            System.out.println("Okey!");
+            System.out.println("Age is greater than 20");
+        }
+ 
+        // Le code sous le bloc 'if'.
+        System.out.println("Done!");
+        
+        
+        
+		if( true )  {
+				   // Faites quelque chose ici
+				}  
+				// D'autre
+				else  { 
+				   // Faites quelque chose ici
+				}
+	}
+	
+	
+	
+
+}
